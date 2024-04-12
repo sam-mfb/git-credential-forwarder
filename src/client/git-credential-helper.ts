f4234a42d323b640081fd6c32e1cf4b155201951
Sam Davidoff <sam@mfbtech.com>
2024-04-12 16:29:15 +0000

    add additional debug statements

--- src/client/git-credential-helper.ts
+++ src/client/git-credential-helper.ts
@@ -18,6 +18,7 @@ export function gitCredentialHelper(args: {
 }): void {
   const debug = args.options?.debugger ? args.options?.debugger : () => {}
 
+  debug(`Extracting git credential operation...`)
   const operationResult = extractOperation(args.argv, {
     expectedLocation: 2
   })
@@ -40,9 +41,12 @@ export function gitCredentialHelper(args: {
   let rawInput = ""
   args.streams.input.setEncoding("utf8")
   args.streams.input.on("data", (data: string) => {
+    debug(`Received input data: ${data}`)
     rawInput += data
     if (rawInput.trim() === "" || rawInput.endsWith("\n\n")) {
+      debug(`Pausing input stream...`)
       args.streams.input.pause()
+      debug(`Running credential operation handler...`)
       runCredentialOperationHandler({
         operation: operation,
         rawInput: rawInput,
@@ -53,6 +57,8 @@ export function gitCredentialHelper(args: {
     }
   })
   args.streams.input.on("end", () => {
+    debug(`Input stream ended`)
+    debug(`Running credential operation handler...`)
     runCredentialOperationHandler({
       operation: operation,
       rawInput: rawInput,
@@ -73,5 +79,5 @@ function runCredentialOperationHandler(args: {
   }
   options?: CredentialHandlerOptions
 }): void {
-  //
+  const debug = args.options?.debugger ? args.options?.debugger : () => {}
 }
