ced00b18be9b611ad9de57e84b4c1ba0c3d990b6
Sam Davidoff <sam@mfbtech.com>
2024-04-15 12:29:04 +0000

    add credential querier

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,12 @@
+import { buildCredentialQuerier } from "./buildCredentialQuerier"
 import { buildCredentialReceiver } from "./buildCredentialReceiver"
 
+const credentialQuerier = buildCredentialQuerier({
+  debugger: str => process.stderr.write(str + "\n")
+})
 const credentialReceiver = buildCredentialReceiver({
   socketPath: "/tmp/cred_rec.sock",
-  credentialOperationHandler: (operation, input) => {
-    return Promise.resolve(input)
-  },
+  credentialOperationHandler: credentialQuerier,
   debugger: str => process.stderr.write(str + "\n")
 })
 
